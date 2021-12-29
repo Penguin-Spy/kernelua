f57747c1fe6bdf176d3d8e0e25726caad1a59cd7
Penguin_Spy <penguinspy2435@outlook.com>
2021-12-28 20:29:57 -0800

    [feat] power shutdown option doesn't fully power off (not possible), but most stuff on the chip is off.

--- src/rpi-power.h
+++ src/rpi-power.h
@@ -15,6 +15,16 @@
 
 #define POWER_PASSWORD  (0x5A << 24)
 
+// from https://github.com/raspberrypi/linux/blob/3b799b23157f8d3961971c5f956aaf79f0bf1de7/drivers/watchdog/bcm2835_wdt.c
+#define POWER_RSTC_WRCFG_CLR		0xffffffcf
+
+#define POWER_RSTS    (POWER_BASE + 0x20)
+#define POWER_RSTS_PARTITION_CLR 0xfffffaaa
+#define POWER_RSTS_RASPBERRYPI_HALT 0x555
+
+// functions
+
 void RPI_PowerReset();
+void RPI_PowerOff();
 
 #endif
